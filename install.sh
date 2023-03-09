ecb8d2296a3f3bb22e81f4c08458bac59d9b57f6
Antonino <antonino.grasso.it@gmail.com>
2023-03-09 16:59:02 +0100

    add zsh theme/config

--- install.sh
+++ install.sh
@@ -14,3 +14,6 @@ chsh -s $(which zsh)
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
