8de2e51dc44113a657036d6aafb3020141a67146
Antonino <antonino.grasso.it@gmail.com>
2023-03-09 16:38:17 +0100

    add ohmyzsh

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@
 
 sudo apt install zsh -y
 zsh --version
+
+# zsh default shell -> log out / log in
+chsh -s $(which zsh)
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
